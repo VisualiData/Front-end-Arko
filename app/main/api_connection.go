9bc086a4b3259572338cdf1f91f72a77acbf7816
Arko Rietdijk <arko.rietdijk@gmail.com>
2017-05-08 11:48:10 +0200

    Changed nav bar

--- app/main/api_connection.go
+++ app/main/api_connection.go
@@ -17,11 +17,18 @@ func get_data(url string) *Result{
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("Authorization", "dev_test")
 	resp, err := client.Do(req)
+	log.Print("before-before")
 	if err != nil {
+		log.Print("before-Err")
 		log.Print(err)
+		log.Print("after-Err")
+		return &Result{Data: ""}
 	}
+	log.Print("before-close")
 	defer resp.Body.Close()
-	resp_body, _ := ioutil.ReadAll(resp.Body)
+	log.Print("before")
+	resp_body, err := ioutil.ReadAll(resp.Body)
+	log.Print("after")
 	return &Result{Data: string(resp_body)}
 	//return string(resp_body)
 }
