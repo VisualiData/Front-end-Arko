f0c3c0a70b78484419a4af6604d4eabe994aae74
Arko Rietdijk <arko.rietdijk@gmail.com>
2017-06-02 14:58:22 +0200

    Load last 2 hours for graph

--- app/main/application.go
+++ app/main/application.go
@@ -6,9 +6,28 @@ import (
 	"html/template"
 	"net/http"
 	"encoding/json"
+	"strconv"
+	"time"
 )
 
+func ToString(value interface{}) string {
+	switch v := value.(type) {
+	case string:
+		return v
+	case int:
+		return strconv.Itoa(v)
+	// Add whatever other types you need
+	default:
+		return ""
+	}
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	then := now.AddDate(0, 0, -2)
+	now_time := now.UTC().Format(time.RFC3339)
+	then_time := then.UTC().Format(time.RFC3339)
+	fmt.Println(now)
 	vars := mux.Vars(r)
 	fmap := template.FuncMap{
 		"marshal": func(v interface {}) template.JS {
@@ -16,7 +35,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 			return template.JS(a)
 		}}
 	// should be dynamic
-	url := BaseUrl + "/sensor/CHIBB-Test-02/2017-05-27T00:00:00/2017-05-29T13:15:00/Temperature"
+	url := BaseUrl + "/sensor/CHIBB-Test-01/" + then_time + "/" + now_time + "/Temperature"
 	if vars["sensor_id"] != "" {
 		url = BaseUrl + "/sensor/"+ vars["sensor_id"]+"/2017-05-27T00:00:00/2017-05-29T13:15:00/Temperature"
 	}
@@ -89,12 +108,22 @@ func edit_sensor_view(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	url := BaseUrl + "/sensor/" + vars["sensor_id"]
 	result := getDataSingle(url)
-	t, _ := template.ParseFiles("dist/index.html", "dist/includes/nav.html", "dist/pages/editsensor.html")
+	t, err := template.New("index.html").Funcs(template.FuncMap{"tostring": ToString}).ParseFiles("dist/index.html", "dist/includes/nav.html", "dist/pages/editsensor.html")
+	if err != nil {
+		fmt.Fprint(w, "Error:", err)
+		fmt.Println("Error:", err)
+		return
+	}
+
 	t.Execute(w, result)
+	//t, _ := template.ParseFiles("dist/index.html", "dist/includes/nav.html", "dist/pages/editsensor.html")
+	//t.Execute(w, result)
 }
 
 func edit_sensor(w http.ResponseWriter, r *http.Request) {
 	location := Position{r.FormValue("x_coordinate"), r.FormValue("y_coordinate"), r.FormValue("floor"), "CHIBB"}
+	fmt.Println(r.FormValue("x_coordinate"));
+	fmt.Println(r.FormValue("y_coordinate"));
 	s := Sensor{r.FormValue("sensor_id"), r.FormValue("sensorType"), r.FormValue("nodeName"), r.FormValue("nodeType"), location, "active"}
 	b, err := json.Marshal(s)
 	if err != nil {
